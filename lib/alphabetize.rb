e806e43fefa527c25bfb543d5fd360309df9cdfd
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-18 04:03:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
  
  arr.sort_by do |word|
-   word[0].each do |alphabet|
+   word.initial.each do |alphabet|
      esperanto.index(alphabet)
    end
  end
