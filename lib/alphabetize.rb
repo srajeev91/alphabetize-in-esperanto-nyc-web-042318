fa6cd5000ba9c84d07e8f8728eb1ccfea8aacac5
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-18 04:23:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |word|
-    word.split("").each do |character|
+    word.split("").collect do |character|
       ESPERANTO_ALPHABET.index(character)
     end
   end
