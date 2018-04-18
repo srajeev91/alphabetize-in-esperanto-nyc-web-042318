97d47be92686f12e5ed4a20e2ba355b1f1c4043a
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-18 04:19:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,9 @@
-esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
- 
- alphabet_array = esperanto.split(" ")
- 
- arr.sort_by do |word|
-   word.split("").each do |character|
-     esperanto.index(character)
-   end
- end
+  arr.sort_by do |phrase|
+    phrase.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
